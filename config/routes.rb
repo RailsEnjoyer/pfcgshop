6bf5620df82c3955ded1538717724ab617b3a294
Bogdan Davidovich <Railsenjoyer@gmail.com>
2023-03-05 01:06:23 +0300

    added controllers and updated db table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/components', to: 'components#create'
+  patch '/components/:id', to: 'components#update'
+  delete '/components/:id', to: 'components#destroy'
 end
--- db/migrate/20230304171407_create_components.rb
+++ db/migrate/20230304171407_create_components.rb
@@ -2,7 +2,7 @@ class CreateComponents < ActiveRecord::Migration[7.0]
   def change
     create_table :components do |t|
       t.text :body
-      t.integer :user_id
+      t.integer :user_id, index: true
 
       t.timestamps
     end
